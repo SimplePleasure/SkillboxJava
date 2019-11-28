2d3be5d5ae3df79f4634904352d0b8e67e81a73e
Konstantin <mf93@mail.ru>
2019-11-28 13:56:24 +0300

    LinkedBlockingDeque learned

---
+++ 13/LinkedBlockingDequeExp/src/Loader.java
@@ -0,0 +1,85 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
+
+
+/*
+Сделано по примерам из:
+https://www.codeflow.site/ru/article/java-blocking-queue
+ */
+
+
+public class Loader
+{
+   ArrayList<String> months;
+   LinkedBlockingDeque<String> deque;
+   final String posionPill = "exit";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //BlockingDeque bd = new BlockingDeque();
+        Loader l = new Loader();
+        l.months = new ArrayList<>();
+
+        l.months.add("Январь");
+        l.months.add("Февраль");
+        l.months.add("Март");
+        l.months.add("Апрель");
+        l.months.add("Май");
+        l.months.add("Июнь");
+        l.months.add("Июль");
+        l.months.add("Август");
+        l.months.add("Сентябрь");
+        l.months.add("Октябрь");
+        l.months.add("Ноябрь");
+        l.months.add("Декабрь");
+
+        l.deque = new LinkedBlockingDeque<>(2);
+        ExecutorService ex = Executors.newFixedThreadPool(2);
+
+        ex.execute(l::producer);
+        ex.execute(l::consumer);
+        ex.shutdown();
+        ex.awaitTermination(1, TimeUnit.MINUTES);
+
+
+
+    }
+    public void producer() {
+
+        Iterator<String> iter = months.iterator();
+
+        try {
+            while (iter.hasNext()) {
+                String month = iter.next();
+                Thread.sleep((int)(Math.random()*1000));
+                deque.put(month);
+            }
+            deque.put(posionPill);
+        } catch (InterruptedException ignore) {}
+    }
+
+    public void consumer() {
+
+        try {
+            while (true) {
+
+                String month = deque.take();
+
+                if (!month.equals("exit")) {
+                    System.out.println(month);
+                    Thread.sleep((int) (Math.random() * 1000));
+                } else {
+                    System.out.println("\n\tDone.");
+                    break;
+                }
+            }
+        } catch (InterruptedException ignore) {}
+    }
+
+}
