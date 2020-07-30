41ffc4d4b6d360208d8c6b8c49567cfb4f809024
SimplePleasure <51758659+SimplePleasure@users.noreply.github.com>
2020-07-30 04:23:24 +0300

    testTask

---
+++ TestTask/src/main/java/VitaSoft/TestTask/Controller.java
@@ -0,0 +1,83 @@
+package VitaSoft.TestTask;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Locale;
+
+
+@RestController
+public class Controller {
+
+    @RequestMapping(value = "/getmonth/{id}", method = RequestMethod.GET)
+    public ResponseEntity<String> getMonth (@PathVariable int id) {
+
+        if (id > 0 && id <= 12) {
+            String month = Month.of(id).getDisplayName(TextStyle.FULL_STANDALONE, new Locale("ru")).toUpperCase().replaceAll("", "-");
+            return ResponseEntity.status(HttpStatus.OK).body(month.substring(1, month.length()-1));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("INCORRECT INPUT DATA");
+        }
+    }
+
+
+
+
+//    @RequestMapping(value = "/sort", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+//    public ResponseEntity<String> list (@RequestBody String text) {
+//        try {
+//            String str = StringEditor.getJson(text);
+//            ArrayList<String> list = new ArrayList<>();
+//
+//            JSONParser parser = new JSONParser();
+//            JSONArray obj = (JSONArray) parser.parse(str);
+//
+//            obj.stream()
+//                    .sorted(Comparator.comparing((s1) -> s1.toString().length()).thenComparing(Object::toString))
+//                    .forEach(x -> list.add("(" + x.toString().length() + "): " + x.toString()));
+//
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            String sortedJson = objectMapper.writeValueAsString(list);
+//            System.out.println(sortedJson);
+//            return ResponseEntity.status(HttpStatus.OK).body(sortedJson);
+//
+//        } catch (ParseException | JsonProcessingException e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//    }
+
+    @RequestMapping(value = "/sort", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    public ResponseEntity<String> list (@RequestParam(name = "text") String str) {
+        try {
+
+            System.out.println(str);
+            ArrayList<String> list = new ArrayList<>();
+
+            JSONParser parser = new JSONParser();
+            JSONArray obj = (JSONArray) parser.parse(str);
+
+            obj.stream()
+                    .sorted(Comparator.comparing((s1) -> s1.toString().length()).thenComparing(Object::toString))
+                    .forEach(x -> list.add("(" + x.toString().length() + "): " + x.toString()));
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String sortedJson = objectMapper.writeValueAsString(list);
+            System.out.println(sortedJson);
+            return ResponseEntity.status(HttpStatus.OK).body(sortedJson);
+
+        } catch (ParseException | JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+}
